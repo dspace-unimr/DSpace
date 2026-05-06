5f69310c12e155225ed26971b5f0cbaf020229d4
Jonas Tschammer <jonas.tschammer@ub.uni-marburg.de>
2026-05-06 08:51:50 +0200

    Applied checkstyle suggestions

--- dspace-api/src/main/java/org/dspace/content/logic/condition/ReadableByGroupCondition.java
+++ dspace-api/src/main/java/org/dspace/content/logic/condition/ReadableByGroupCondition.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.dspace.authorize.ResourcePolicy;
 import org.dspace.authorize.factory.AuthorizeServiceFactory;
 import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.Item;
